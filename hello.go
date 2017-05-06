e6bc5a915e97ac089ff136564a152d6ac22ebc31
allenbsf <allenbsf@Allens-MacBook-Pro.local>
2017-05-06 12:12:24 -0700

    initial commit

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "fmt"
+  "time"
+  )
+
+func main() {
+  fmt.Println("Hello World!")
+  fmt.Println("This is my Go Playground")
+  fmt.Println("The time is", time.Now())
+  fmt.Println("test")
+}
